b65572230c5d87aad5f26e74e223716e188520a7
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-16 21:34:21 +1000

    Исправил настройку хоста для базы данных.

--- backend/expense_tracker/settings.py
+++ backend/expense_tracker/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'expense_tracker'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', 'tracker_database'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
