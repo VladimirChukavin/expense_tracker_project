af107be9ee0d72e3d1b8c6a8943f2c9185b2f67f
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-17 23:06:14 +1000

    Удалил ненужную строку, изменил название приложения.

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -5,6 +5,5 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    verbose_name = 'Core'
+    verbose_name = 'Общие компоненты'
