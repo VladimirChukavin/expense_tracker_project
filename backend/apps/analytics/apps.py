66d9d6d4fe9cebddee4a1989dccbcd9cd45b2e2b
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-17 23:05:28 +1000

    Удалил ненужную строку, изменил название приложения.

--- backend/apps/analytics/apps.py
+++ backend/apps/analytics/apps.py
@@ -5,6 +5,5 @@ from django.apps import AppConfig
 
 
 class AnalyticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.analytics'
-    verbose_name = 'Analytics'
+    verbose_name = 'Аналитика'
