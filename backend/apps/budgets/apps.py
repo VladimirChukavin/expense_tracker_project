5df0365f2ad26b6b9aae5427ebfccab702bcdf5a
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-17 23:05:36 +1000

    Удалил ненужную строку, изменил название приложения.

--- backend/apps/budgets/apps.py
+++ backend/apps/budgets/apps.py
@@ -5,6 +5,5 @@ from django.apps import AppConfig
 
 
 class BudgetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.budgets'
-    verbose_name = 'Budgets'
+    verbose_name = 'Бюджеты'
