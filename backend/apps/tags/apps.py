f682e8d66050d92c5b4155ac0fa13e7d211961d8
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-17 23:06:03 +1000

    Удалил ненужную строку, изменил название приложения.

--- backend/apps/tags/apps.py
+++ backend/apps/tags/apps.py
@@ -5,6 +5,5 @@ from django.apps import AppConfig
 
 
 class TagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tags'
-    verbose_name = 'Tags'
+    verbose_name = 'Тег'
