927944d3266fa314175420ba361caf503961652b
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-20 15:06:53 +1000

    Создал файл PrivateRoute.tsx.

---
+++ frontend/src/routes/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import {Navigate} from 'react-router-dom';
+import {useAuthStore} from '@/stores/authStore';
+import * as React from 'react';
+
+interface PrivateRouteProps {
+    children: React.ReactNode;
+}
+
+export const PrivateRoute = ({children}: PrivateRouteProps) => {
+    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace/>;
+    }
+
+    return <>{children}</>;
+};
