3dcd67a0b8ac260ca635a4511412bea52df59003
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-20 17:21:13 +1000

    Изменил свойства объекта User.

--- frontend/src/types/models.ts
+++ frontend/src/types/models.ts
@@ -4,11 +4,11 @@ export interface User {
   id: number;
   email: string;
   username: string;
-  first_name: string;
-  last_name: string;
-  default_currency: number;
-  created_at: string;
-  updated_at: string;
+  first_name?: string;
+  last_name?: string;
+  default_currency?: number;
+  created_at?: string;
+  updated_at?: string;
 }
 
 export interface Currency {
