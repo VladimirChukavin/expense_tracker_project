f4a1129df9e53ea57cb72a65e4a7e115f087da51
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:12:10 +1000

    style(users): удалил docstrings из serializers.py

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,6 +1,3 @@
-"""
-User serializers for authentication and profile.
-"""
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
@@ -9,9 +6,6 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for user model.
-    """
     class Meta:
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'is_active', 'date_joined')
@@ -19,9 +13,6 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    """
-    Serializer for user registration.
-    """
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
@@ -30,28 +21,32 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'password2', 'first_name', 'last_name')
 
     def validate(self, attrs):
-        """Validate that passwords match."""
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Passwords do not match."})
         return attrs
 
     def create(self, validated_data):
-        """Create user with hashed password."""
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    """
-    Serializer for password change.
-    """
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True, validators=[validate_password])
     new_password2 = serializers.CharField(required=True)
 
     def validate(self, attrs):
-        """Validate that new passwords match."""
         if attrs['new_password'] != attrs['new_password2']:
             raise serializers.ValidationError({"new_password": "Passwords do not match."})
         return attrs
+
+
+class DeleteAccountSerializer(serializers.Serializer):
+    password = serializers.CharField(required=True)
+
+    def validate_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Wrong password.")
+        return value
