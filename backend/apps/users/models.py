737a60070e9ce21eeaf68c684d9d60cb195ae814
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:11:54 +1000

    style(users): удалил docstrings из models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,18 +1,11 @@
-"""
-Custom user model.
-"""
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-    """
-    Custom user manager where email is the unique identifier.
-    """
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create and save a user with the given email and password."""
         if not email:
             raise ValueError(_('The Email field must be set'))
         email = self.normalize_email(email)
@@ -22,7 +15,6 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        """Create and save a superuser with the given email and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -36,9 +28,6 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    """
-    Custom user model using email as username.
-    """
     username = None
     email = models.EmailField(_('email address'), unique=True)
 
