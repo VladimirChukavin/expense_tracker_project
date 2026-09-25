0dd8ea706b8fb3a62bd16bd7f95e3231df92e296
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:28:01 +1000

    style(core): удалил docstrings из permissions.py

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -1,13 +1,7 @@
-"""
-Core permissions for API views.
-"""
 from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view/edit it.
-    """
 
     def has_object_permission(self, request, view, obj):
         """
