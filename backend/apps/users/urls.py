49e34d9e50ac0e1f75ab848a2cf9972d40e52226
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:12:24 +1000

    style(users): удалил docstring из urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,12 +1,11 @@
-"""
-User URLs configuration.
-"""
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
+    LoginView,
     RegisterView,
     ProfileView,
     ChangePasswordView,
+    logout,
     delete_account
 )
 
@@ -15,10 +14,12 @@ app_name = 'users'
 urlpatterns = [
     # Authentication
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', logout, name='logout'),
 
     # Profile
+    path('me/', ProfileView.as_view(), name='me'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('delete-account/', delete_account, name='delete-account'),
