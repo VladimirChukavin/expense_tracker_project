b4650a44634f2e00305d8f763acc6d2e9247c6ce
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:07:28 +1000

    style(currencies): удалил docstring из urls.py

--- backend/apps/currencies/urls.py
+++ backend/apps/currencies/urls.py
@@ -1,6 +1,3 @@
-"""
-Currency URLs configuration.
-"""
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CurrencyViewSet, ExchangeRateViewSet
