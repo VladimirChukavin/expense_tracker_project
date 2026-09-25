ea7d6c0996771dd80aaec647907c45efd2fd3251
VladimirChukavin <dartmorgot@yandex.ru>
2026-09-25 19:09:23 +1000

    style(expenses): удалил docstring из urls.py

--- backend/apps/expenses/urls.py
+++ backend/apps/expenses/urls.py
@@ -1,6 +1,3 @@
-"""
-Expense URLs configuration.
-"""
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ExpenseViewSet, RecurringExpenseViewSet
